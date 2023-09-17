6b9235d62206f926148a51f9da516918c876675a
KingYano <belkadi.yanis.l@gmail.com>
2023-09-17 19:25:34 +0200

    POC typescript with vue3

---
+++ typescriptvue/src/App.vue
@@ -0,0 +1,28 @@
+
+
+<template>
+
+  <div>
+      <HelloWorld
+        :items= "items"
+      />
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import type User from "../src/types/User";
+
+const items = ref<User[]>([
+    {
+      name: "Jean",
+      age: 23
+    },
+    {
+      name: "Lucie",
+      age: 25
+    },
+  ])
+</script>
---
+++ typescriptvue/src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+
+
+<template>
+  <div>
+    <h1
+      v-text="title"
+    ></h1>
+
+    <h2
+      v-text="subtitle"
+    ></h2>
+
+    <button
+      v-on:click="editTitle()"
+    >
+      Editer title
+    </button>
+
+    <button
+      v-on:click="editSubtitle()"
+    >
+      Editer subtitle
+    </button>
+
+
+    <div v-for="item in props.items" :key="item">
+      <span
+        v-text="item.name"
+      ></span>
+      <span
+        v-text="item.age"
+      ></span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import User from "../types/User";
+
+  const props = defineProps<{
+    items: User[],
+  }>();
+
+  const title = ref<string>('Hello World');
+  const subtitle = ref<string | number>('75');
+
+  const editTitle = (editTitle: string) => {
+    title.value = "Hello There"
+  };
+
+  const editSubtitle = (editSubtitle: string | number) => {
+    subtitle.value = 95
+  };
+
+</script>
